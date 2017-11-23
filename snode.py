81fc3f0f8d6be30ffcccd71d68c111365db791e2
khang4 <khang4@umbc.edu>
2017-11-23 13:46:23 -0500

    snode now only has values for leaf nodes so it makes more sense (?)

--- snode.py
+++ snode.py
@@ -1,19 +1,33 @@
 import random;
 
 class snode:
+    id=0;
+
     # constructor takes parent node, and max randomised
     # value for the node
     def __init__(self,parent=0,maxValue=100):
+        snode.id+=1;
+        self.id=snode.id;
         self.value=random.randint(0,maxValue);
         self.parent=parent;
         self.children=[];
 
     def __str__(self):
-        return "n"+str(self.value);
+        return "n"+str(self.id);
 
-    #advanced str representation: {node value} ({parent value},{number of children})
+    #advanced string print
+    #prints out n<node id number> (p:<parent node>,c:<number of children>,v:<value of node>)
+    #if node has no children or no value (for non leaf nodes) then it won't print out c or v
     def __repr__(self):
-        return "n{} ({},{})".format(self.value,str(self.parent),len(self.children));
+        childrenString="";
+        if len(self.children)>0:
+            childrenString=",c:{}".format(len(self.children));
+
+        valueString="";
+        if self.value>-1:
+            valueString=",v:{}".format(self.value);
+
+        return "n{} (p:{}{}{})".format(self.id,str(self.parent),childrenString,valueString);
 
     # generate children array for this node with maximum
     # amount of children and value, return the list of children
@@ -23,6 +37,10 @@ class snode:
         for x in range(random.randint(0,maxChildren)):
             newchildren.append(snode(self,maxValue));
         self.children=newchildren;
+
+        if len(newchildren)>0:
+            self.value=-1;
+
         return newchildren;
 
 # generate a tree with given parameters
