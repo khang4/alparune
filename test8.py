149f5b04c3dd58364b81caeba11af422cb5c8af4
khang4 <khang4@umbc.edu>
2017-12-04 02:52:04 -0500

    more graphs/tests

--- test8.py
+++ test8.py
@@ -3,9 +3,11 @@
 import mmax;
 import nemax;
 import snode;
-import timeit;
+import psutil;
+import os;
 
 def main():
+    pr=psutil.Process(os.getpid());
     #changeable variables:
     # maxHeight=5;
     # minNodes=10000;
@@ -17,9 +19,9 @@ def main():
     maxValue=100;
     gentreeProgress=1;
 
-    filename="test1b.txt";
+    filename="test8d.txt";
 
-    for x in range(14,15):
+    for x in range(0,15):
         with open(filename,"a") as ofile:
             #put things that change here:
             maxHeight=x;
@@ -30,26 +32,13 @@ def main():
             # tree=snode.genGraphTree(minNodes,maxNodes,minChildren,maxChildren,maxValue,gentreeProgress);
 
             #DONT CHANGE ANYTHING BELOW HERE!!!!
-            times=[0,0,0,0];
-
-            times[0]=timeit.default_timer();
-            mmax.mmax(tree,0,maxValue);
-            times[0]=timeit.default_timer()-times[0];
-
-            times[1]=timeit.default_timer();
-            mmax.alphaMax(tree,0,maxValue);
-            times[1]=timeit.default_timer()-times[1];
-
-            times[2]=timeit.default_timer();
-            nemax.nemax(tree,1,maxValue);
-            times[2]=timeit.default_timer()-times[2];
-
-            times[3]=timeit.default_timer();
+            # mmax.mmax(tree,0,maxValue);
+            # mmax.alphaMax(tree,0,maxValue);
+            # nemax.nemax(tree,1,maxValue);
             nemax.nescout(tree,1,maxValue);
-            times[3]=timeit.default_timer()-times[3];
 
             del tree;
-            ofile.write("{} {} {} {} {}\n".format(x,times[0],times[1],times[2],times[3]));
+            ofile.write("{} {}\n".format(x,pr.memory_full_info().uss));
 
 if __name__=="__main__":
     main();
